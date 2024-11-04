a7a6f8f7518a33a89a0d3cebfea6bbca3d2638dc
New Author Name <priv.luk@gmail.com>
2024-11-05 00:24:48 +0530

    vili: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -22,13 +22,10 @@ PRODUCT_NAME := lineage_vili
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_SYSTEM_NAME := vili_global
-PRODUCT_SYSTEM_DEVICE := vili
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vili-user 14 UKQ1.231207.002 V816.0.4.0.UKDEUXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/vili/vili:14/UKQ1.231207.002/V816.0.4.0.UKDEUXM:user/release-keys
+    BuildDesc="vili-user 14 UKQ1.231207.002 V816.0.4.0.UKDEUXM release-keys" \
+    BuildFingerprint=Xiaomi/vili/vili:14/UKQ1.231207.002/V816.0.4.0.UKDEUXM:user/release-keys \
+    DeviceName=vili \
+    DeviceProduct=vili \
+    SystemDevice=vili \
+    SystemName=vili
