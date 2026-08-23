05090a625b118d1518a99061c225e4ffed28dd62
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-08-23 05:53:57 +0530

    vili: Include DeviceAsWebcam
    - Required after https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/4e3d3ee092e2c3114123ea9f345f1dd3ff748de1
    
    Change-Id: I9096d3c2f0afae9f08a8dc88ccd932dc2d3f4573

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -37,4 +37,5 @@ TARGET_INCLUDES_Custom_Thermal := true
 TARGET_INCLUDES_HIGH_TOUCH_POLLING := true
 TARGET_INCLUDES_Lineage_Health := true
 TARGET_INCLUDES_Torch_Light_Control := true
+TARGET_INCLUDES_Webcam := true
 
